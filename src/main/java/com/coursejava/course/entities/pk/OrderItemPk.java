ec4bcef0e52e515f73ca0a2df07bfb1e5c153c90
Antonio Miquelini <antoniomiquelini@gmail.com>
2022-03-23 09:00:28 -0300

    Erro Aula 337

---
+++ src/main/java/com/coursejava/course/entities/pk/OrderItemPk.java
@@ -0,0 +1,56 @@
+package com.coursejava.course.entities.pk;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import com.coursejava.course.entities.Order;
+import com.coursejava.course.entities.Product;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToMany
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	@ManyToMany
+	@JoinColumn(name = "product_id")
+	private Product product;
+	
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItemPk other = (OrderItemPk) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+	
+	
+
+}
